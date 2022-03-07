e129b4dcd9d36256c8a373d52e313af75f23fdca
Ehis <edemaehiz@gmail.com>
2022-03-07 10:54:44 +0100

    edited about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,10 @@ export const About = () => {
   return (
     <div>
         <h4> Version 1.0.0  </h4>
+        <p> A Task Tracker built with React by Ehizman</p>
+        <p> An introduction to working with React. The Porject help me undertsstand how to use the React UseState hooks
+            and also the Reacr Router dom
+        </p>
         <Link to="/"> Go back </Link>
     </div>
   )
